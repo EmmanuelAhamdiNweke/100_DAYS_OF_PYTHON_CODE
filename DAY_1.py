f7b5b6530cc2e949cc8df7561e73749a72a2ba80
EmmanuelAhamdiNweke <nzexx@yahoo.co.uk>
2025-01-22 12:47:41 +0100

    time added

--- DAY_1.py
+++ DAY_1.py
@@ -1,12 +1,25 @@
+
 # Welcome Message Generator
+from datetime import datetime
+current_time = datetime.now()
+hour = current_time.hour
 
 # Step1: Ask for user details
-name = input("what's your name? ")
+name = input("what's your name? ").capitalize()
 hobby = input("what's your favorite hobby? ")
 
+if hour >= 5 and hour < 12:
+    greeting = "Good morning"
+elif hour >= 12 and hour < 16:
+    greeting = "Good afternoon"
+elif hour >= 16 and hour < 22:
+    greeting = "Good evening"
+else:
+    greeting = "Good night"     
+print(hour)
 # Step 2: Generate a personalized welcome message
 print("\n--- Welcome Message ---")
-print(f"Hellow, {name}! ")
+print(f"{greeting}, {name}! ")
 print(f"Welcome to the World of Python Programming,")
 print(f"It's great to know that you love {hobby}.")
 print(f"Get ready to build something amazing today.")
