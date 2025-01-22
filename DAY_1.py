2cff4eff961a4a883f5a32df9a039d55677272a9
EmmanuelAhamdiNweke <nzexx@yahoo.co.uk>
2025-01-22 12:21:45 +0100

    first day

---
+++ DAY_1.py
@@ -0,0 +1,12 @@
+# Welcome Message Generator
+
+# Step1: Ask for user details
+name = input("what's your name? ")
+hobby = input("what's your favorite hobby? ")
+
+# Step 2: Generate a personalized welcome message
+print("\n--- Welcome Message ---")
+print(f"Hellow, {name}! ")
+print(f"Welcome to the World of Python Programming,")
+print(f"It's great to know that you love {hobby}.")
+print(f"Get ready to build something amazing today.")
