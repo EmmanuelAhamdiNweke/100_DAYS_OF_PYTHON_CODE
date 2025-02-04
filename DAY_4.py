3120b22d6a2db4c8dd1933d493632d065dd4738a
EmmanuelAhamdiNweke <nzexx@yahoo.co.uk>
2025-02-04 10:16:39 +0100

    compare three numbers

--- DAY_4.py
+++ DAY_4.py
@@ -1,20 +1,78 @@
-# Step 1: Get use input for two numbers
+# Step 1: Get use input for the numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number "))
+num3 = float(input("Enter the third number"))
+
+# positive= if greater than 0
+# negative if less than 0
+# zero
+   
+if num1 > 0:
+    print (f"{num1} is positive")
+elif num1 < 0:
+    print (f"{num1} is negative")
+else:
+    print (f"{num1} is zero")
+
+if num2 > 0:
+    print (f"{num2} is positive")
+elif num2 < 0:
+    print (f"{num2} is negative")
+else:
+    print (f"{num2} is zero")
+
+if num3 > 0:
+    print (f"{num3} is positive")
+elif num3 < 0:
+    print (f"{num3} is negative")
+else:
+    print (f"{num3} is zero")
+
+
+
+
+
+
+
+
+
+
+
+
+# if num1>0:
+#     print(f"{num1} is positive")
+# elif num1<0:
+#     print(f"{num1} is negative")
+# else:
+#     print(f"{num1} is zero")
+
+# if num2>0:
+#     print(f"{num2} is positive")
+# elif num2<0:
+#     print(f"{num2} is negative")
+# else:
+#     print(f"{num2} is zero")
+
+# if num3>0:
+#     print(f"{num3} is positive")
+# elif num3<0:
+#     print(f"{num3} is negative")
+# else:
+#     print(f"{num3} is zero")
 
 # Step 2: Compare the numbers
 print("\n--- comarison Results ---")
 
-if num1 == num2:
-    print(f"Both numbers are equal: {num1}")
-elif num1 > num2:
-    print(f"{num1} is greater than {num2}")
-else:
-    print(f"{num2} is greater than {num1}")
+# if num1 == num2 :
+#     print(f"Both numbers are equal: {num1}")
+# elif num1 > num2:
+#     print(f"{num1} is greater than {num2}")
+# else:
+#     print(f"{num2} is greater than {num1}")
 
-# Step 3: Check if any number is zero
-if num1 == 0 or num2 == 0:
-    print("\nAt least one number is zero.")
-else:
-    print("\nBoth numbers are non-zero.")
+# # Step 3: Check if any number is zero
+# if num1 == 0 or num2 == 0:
+#     print("\nAt least one number is zero.")
+# else:
+#     print("\nBoth numbers are non-zero.")
 
