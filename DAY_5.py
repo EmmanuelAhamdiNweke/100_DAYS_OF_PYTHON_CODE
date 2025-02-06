1e0cea58c5f76f5a32b567f1a64cd9059da9f253
EmmanuelAhamdiNweke <nzexx@yahoo.co.uk>
2025-02-06 08:20:00 +0100

    halfway and emoji added

--- DAY_5.py
+++ DAY_5.py
@@ -29,7 +29,8 @@ import time
 # Asks user for input and convert the input to an integer and store in a variable named 'start'
 
 start = int(input("Enter the number to start countdown "))
-
+speed = int(input("Enter speed for countdown "))
+halfway = round(start / 2)
 print("\n--- Countdown Begins ---")
 # Step3
 # Write a while loop that check for inputs as long as the input is less than 0
@@ -37,15 +38,17 @@ print("\n--- Countdown Begins ---")
 while start > 0:
 # Step4
 # Print the input(i.e the start variable) for every iteration
-
+    
     print (start)
+        
 # Step 5
 # Using the time library, pause the execution for at least 1 sec
-    time.sleep(2)
+    time.sleep(speed)
 # Step 6
 # After each iteration, subtract 1 from the start variable.
     start -= 1
-
+    if halfway == start:
+        print('halfway')
 # Step 7
 # Print "Countdown Completed"
-print ("Countdown Completed")
+print ("Countdown Completed 🎉")
