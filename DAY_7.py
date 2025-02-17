5aefc327582af780448aa715c8d1fcca2feef227
EmmanuelAhamdiNweke <nzexx@yahoo.co.uk>
2025-02-17 22:09:36 +0100

    allow more items to be added to the list

--- DAY_7.py
+++ DAY_7.py
@@ -1,7 +1,7 @@
 # Shopping List App
 
 # Step 1: Innitialise an empty shopping list
-shopping_list = ["Egg"]
+shopping_list = ["Egg", "Milk", "Bread", "Butter"]
 
 # Step 2: Define the main menu
 def show_menu():
@@ -11,6 +11,11 @@ def show_menu():
     print("3. Remove an item")
     print("4. Clear list")
     print("5. Exit")
+
+def add_items_to_list(shopping_list, *items):
+    shopping_list.extend(items)
+    for item in items:
+        print(f"{item} has been added to the shopping list.")
 
 # Step 3: Main program Loop
 while True:
@@ -27,9 +32,13 @@ while True:
         break
             
     elif choice == "2":
-        item = input("Enter the item to add: ")
-        shopping_list.append(item)
-        print(f"{item} has been added to the shoping list.")
+        # item = input("Enter the item to add: ")
+        # shopping_list.append(item)
+        items = input("Enter the items to add (separate by commas): ")
+        # Split the input string by commas, strip spaces, and add each item to the list
+        item_list = [item.strip() for item in items.split(',')]
+        add_items_to_list(shopping_list, *item_list)
+        print(f"Items {', '.join(item_list)} have been added to the shopping list.")
         print(shopping_list)
         break
 
@@ -38,6 +47,7 @@ while True:
         if item in shopping_list:
             shopping_list.remove(item)
             print(f"{item} has been removed from the shoping_list.")
+            print(shopping_list)
 
         else:
             print(f"{item} is not in the shipping list.")
@@ -47,6 +57,7 @@ while True:
     elif choice == "4":
         shopping_list.clear()
         print("The shopping list has been cleared.")
+        print(shopping_list)
         break
 
     elif choice == "5":
